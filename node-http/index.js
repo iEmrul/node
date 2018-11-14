e030e3c5f4fcfd7d14ab1a1020a258e0de3ffacc
iEmrul <emrul.islam@nih.gov>
2018-11-14 13:27:24 -0500

    node express

--- node-http/index.js
+++ node-http/index.js
@@ -1,11 +1,36 @@
 const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
+const fs = require('fs');
+const path = require('path');
+
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body>hello</body></html>')
+    console.log("reqquest for " + req.url + 'by method ' + req.method);
+    if (req.method == 'GET') {
+        var fileUrl;
+        if (req.url == '/') {
+            fileUrl = '/index.html';
+        } else {
+            fileUrl = req.url;
+        }
+        var filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+        if (fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                if (!exists) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html><body> Error </body></html>');
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+            })
+        }
+    } else {
+
+    }
 })
 
 server.listen(port, hostname, () => {
